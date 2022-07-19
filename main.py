81d643fb017e93e4e4acda606b9a749dcd5e7699
hxs8x9s <KokoLove5790!!>
2022-07-19 21:23:48 +0300

    test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello!".format(name)
+    return "Hello my Koko!".format(name)
 
 
 if __name__ == "__main__":
